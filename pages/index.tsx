5158409b750f2c4c82b4975f0f6d753d3f9a303f
Giorgi Mosemgvdlishvili <g_mosemgvdlishvili@yahoo.com>
2022-01-12 22:21:51 +0400

    fetch data

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,20 @@ import axios from "axios";
 import {API_URL} from "../config";
 import {wrapper} from "../redux/store";
 import {fetchingData} from "../redux/actions";
+import {useSelector} from "react-redux";
+import {fetchData} from "../redux/reducer/fetchdata";
 
-const Home: NextPage = ({data}: any) => {
-    console.log(data)
+const Home: NextPage = () => {
+    const {products} = useSelector((data: any) => data.fetchData)
+    console.log(products)
 
     return (
         <div>
-
+            {products?.map((el: any) => (
+                <div key={el.id}>
+                    <p>{el.title}</p>
+                </div>
+            ))}
         </div>
     )
 }
