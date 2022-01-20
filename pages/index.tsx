67b3311fc85786ea0a14c174aa48cf5872ef20c6
Giorgi Mosemgvdlishvili <g_mosemgvdlishvili@yahoo.com>
2022-01-20 16:31:40 +0400

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type {GetServerSideProps, NextPage} from 'next'
+import type {NextPage} from 'next'
 import Layout from "../components/Layout";
 import Image from "next/image";
 import image from '../public/img.jpg'
